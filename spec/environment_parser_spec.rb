05dbd80f14b1d554aca0899878f97689ff788a87
Hugo Tunius <hugo.tunius@gmail.com>
2015-03-30 23:59:51 +0200

    [Spec] Refactor to be more in line with Rspec recommendations

--- spec/environment_parser_spec.rb
+++ spec/environment_parser_spec.rb
@@ -3,18 +3,24 @@ require_relative '../lib/environment_parser.rb'
 
 module Travish
   RSpec.describe EnvironmentParser do
-    let(:global_env) do
-      [
-        'KEY1=VALUE1 KEY2=VALUE2',
-        { 'KEY3' => 'VALUE3' },
-        'KEY_WITH_QUOTED_VALUE="QUOTED"'
-      ]
-    end
+    describe '.environment_hash' do
+      let(:global_env) do
+        [
+          'KEY1=VALUE1 KEY2=VALUE2',
+          { 'KEY3' => 'VALUE3' },
+          'KEY_WITH_QUOTED_VALUE="QUOTED"'
+        ]
+      end
 
-    describe 'without override' do
-      let(:parser) { described_class.new(global_env) }
+      let(:overrides) do
+        [
+          { 'KEY1' => 'V1', 'KEY2' => 'V2', 'KEY3' => 'not V3' },
+          { 'KEY3' => 'V3' }
+        ]
+      end
 
-      it 'produces the correct environment_hash' do
+      it 'produces the correct result without overrides' do
+        parser = described_class.new(global_env)
         hash = parser.environment_hash
 
         expect(hash).to eq('KEY1' => 'VALUE1',
@@ -22,29 +28,17 @@ module Travish
                            'KEY3' => 'VALUE3',
                            'KEY_WITH_QUOTED_VALUE' => 'QUOTED')
       end
-    end
 
-    describe 'with override' do
-      let(:overrides) do
-        [
-          { 'KEY1' => 'V1', 'KEY2' => 'V2', 'KEY3' => 'not V3' },
-          { 'KEY3' => 'V3' }
-        ]
-      end
-      let(:parser) { described_class.new(global_env, *overrides) }
-
-      it 'produces the corect environment_hash' do
+      it 'produces the corect result with overrides' do
+        parser = described_class.new(global_env, *overrides)
         hash = parser.environment_hash
 
         expect(hash).to eq('KEY1' => 'V1', 'KEY2' => 'V2',
                            'KEY3' => 'V3', 'KEY_WITH_QUOTED_VALUE' => 'QUOTED')
       end
-    end
 
-    describe 'string environment' do
-      let(:parser) { described_class.new('KEY=VALUE OTHER_KEY="OTHER_VALUE"') }
-
-      it 'produces the correct environment_hash' do
+      it 'produces the correct result with a single string as the environment' do
+        parser = described_class.new('KEY=VALUE OTHER_KEY="OTHER_VALUE"')
         hash = parser.environment_hash
 
         expect(hash).to eq('KEY' => 'VALUE', 'OTHER_KEY' => 'OTHER_VALUE')
